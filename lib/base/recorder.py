4596c96b11b79a4b41fde4c78df6a5266e00c5bc
He Ma <hma02@users.noreply.github.com>
2016-04-22 19:42:13 -0400

    Fix a loading bug when resume training
    Fix a loading bug when resume training
    adjust show() function structure for single section display

--- lib/base/recorder.py
+++ lib/base/recorder.py
@@ -177,7 +177,12 @@ class Recorder(object):
         
         with open(filepath, 'rb') as f:
 
-            self.info_dict = pickle.load(f)
+             load_dict= pickle.load(f)
+             self.info_dict['train_info'].extend(load_dict['train_info'])
+             self.info_dict['val_info'].extend(load_dict['val_info'])
+             self.info_dict['epoch_time'].extend(load_dict['epoch_time'])
+             self.info_dict['all_time'].extend(load_dict['all_time'])
+             self.info_dict['lr'].extend(load_dict['lr'])
             
     def show(self, label='', color_id = 0, show=True):
         
@@ -189,126 +194,132 @@ class Recorder(object):
         fontP.set_size('small')
         color = ['-r','-b','-m','-g', '--r','--b','--m']
         
-        count_train, train_loss, train_error  = np.transpose(self.info_dict['train_info'])
-        train_loss = train_loss[:int(train_loss.shape[0]/250) * 250]
-        train_error = train_error[:int(train_error.shape[0]/250) * 250]
-        train_loss = np.mean(train_loss.reshape(-1, 250), axis=1)
-        train_error = np.mean(train_error.reshape(-1, 250), axis=1)
-        
-        count_val, val_loss, val_error, val_error_top5  = np.transpose(self.info_dict['val_info'])
-        count_t, t_all, t_calc, t_comm, t_wait = np.transpose(self.info_dict['all_time'])
-        t_all = t_all[:int(t_all.shape[0]/250) * 250]
-        t_calc = t_calc[:int(t_calc.shape[0]/250) * 250]
-        t_comm = t_comm[:int(t_comm.shape[0]/250) * 250]
-        t_wait = t_wait[:int(t_wait.shape[0]/250) * 250]
-        
-        t_all = np.mean(t_all.reshape(-1, 250), axis=1)
-        t_calc = np.mean(t_calc.reshape(-1, 250), axis=1)
-        t_comm = np.mean(t_comm.reshape(-1, 250), axis=1)
-        t_wait = np.mean(t_wait.reshape(-1, 250), axis=1)
-        
-        count_epoch, t_epoch = np.transpose(self.info_dict['epoch_time'])
-        count_lr , lr = np.transpose(self.info_dict['lr'])
+        if self.info_dict['train_info']!=[]:
+        	
+        	count_train, train_loss, train_error  = np.transpose(self.info_dict['train_info'])
+        	train_loss = train_loss[:int(train_loss.shape[0]/250) * 250]
+        	train_error = train_error[:int(train_error.shape[0]/250) * 250]
+        	train_loss = np.mean(train_loss.reshape(-1, 250), axis=1)
+        	train_error = np.mean(train_error.reshape(-1, 250), axis=1)
+        	
+        	# train error
+        	fig = plt.figure(1, figsize=(5,8))
+        	fig.subplots_adjust(left = 0.15, bottom = 0.07,
+        	                    right = 0.94, top = 0.94,
+        	                    hspace = 0.14)
 
-        # train error
-        fig = plt.figure(1, figsize=(5,8))
-        fig.subplots_adjust(left = 0.15, bottom = 0.07,
-                            right = 0.94, top = 0.94,
-                            hspace = 0.14)
-
-        ax = plt.subplot(211) # one record per 40 iterations , total 250 recordings for 10008 iterations in an epoch
-        ax.plot(train_loss, color[0+color_id], label=self.config['name']+ label)
-        ax.legend(loc='upper right',prop=fontP)
-        #ax.set_xlabel('epoch')
-        ax.set_ylabel('loss')
+	        ax = plt.subplot(211) # one record per 40 iterations , total 250 recordings for 10008 iterations in an epoch
+	        ax.plot(train_loss, color[0+color_id], label=self.config['name']+ label)
+	        ax.legend(loc='upper right',prop=fontP)
+	        #ax.set_xlabel('epoch')
+	        ax.set_ylabel('loss')
         
 
-        ax = plt.subplot(212)
-        ax.plot(train_error, color[0+color_id], label=self.config['name']+ label)
-        ax.legend(loc='upper right',prop=fontP)
-        ax.set_xlabel('epoch')
-        ax.set_ylabel('error')
+	        ax = plt.subplot(212)
+	        ax.plot(train_error, color[0+color_id], label=self.config['name']+ label)
+	        ax.legend(loc='upper right',prop=fontP)
+	        ax.set_xlabel('epoch')
+	        ax.set_ylabel('error')
+	        
+	        
+	        plt.suptitle('training info')
+	        fig.savefig('train.png',format='png')
         
+        if self.info_dict['val_info']!=[]:
+        	count_val, val_loss, val_error, val_error_top5  = np.transpose(self.info_dict['val_info'])
+        	
+        	# val error
+	        fig = plt.figure(2, figsize=(5,8))
+	        fig.subplots_adjust(left = 0.15, bottom = 0.07,
+	                            right = 0.94, top = 0.94,
+	                            hspace = 0.14)
+	
+	        ax = plt.subplot(311) # one record per epoch
+	        ax.plot(val_loss, color[0+color_id], label=self.config['name']+ label)
+	        ax.legend(loc='upper right',prop=fontP)
+	        #ax.set_xlabel('epoch')
+	        ax.set_ylabel('loss')
+	
+	        ax = plt.subplot(312)
+	        ax.plot(val_error, color[0+color_id], label=self.config['name']+ label)
+	        ax.legend(loc='upper right',prop=fontP)
+	        #ax.set_xlabel('epoch')
+	        ax.set_ylabel('error')
+	
+	        ax = plt.subplot(313)
+	        ax.plot(val_error_top5, color[0+color_id], label=self.config['name']+ label)
+	        ax.legend(loc='upper right',prop=fontP)
+	        ax.set_xlabel('epoch')
+	        ax.set_ylabel('top5 error')
+	        
+	        plt.suptitle('validation info')
+	        fig.savefig('val.png',format='png')
         
-        plt.suptitle('training info')
-        fig.savefig('train.png',format='png')
-
-        # val error
-        fig = plt.figure(2, figsize=(5,8))
-        fig.subplots_adjust(left = 0.15, bottom = 0.07,
-                            right = 0.94, top = 0.94,
-                            hspace = 0.14)
-
-        ax = plt.subplot(311) # one record per epoch
-        ax.plot(val_loss, color[0+color_id], label=self.config['name']+ label)
-        ax.legend(loc='upper right',prop=fontP)
-        #ax.set_xlabel('epoch')
-        ax.set_ylabel('loss')
-
-        ax = plt.subplot(312)
-        ax.plot(val_error, color[0+color_id], label=self.config['name']+ label)
-        ax.legend(loc='upper right',prop=fontP)
-        #ax.set_xlabel('epoch')
-        ax.set_ylabel('error')
-
-        ax = plt.subplot(313)
-        ax.plot(val_error_top5, color[0+color_id], label=self.config['name']+ label)
-        ax.legend(loc='upper right',prop=fontP)
-        ax.set_xlabel('epoch')
-        ax.set_ylabel('top5 error')
+        if self.info_dict['all_time']!=[]:
+        	count_t, t_all, t_calc, t_comm, t_wait = np.transpose(self.info_dict['all_time'])
+        	t_all = t_all[:int(t_all.shape[0]/250) * 250]
+        	t_calc = t_calc[:int(t_calc.shape[0]/250) * 250]
+        	t_comm = t_comm[:int(t_comm.shape[0]/250) * 250]
+        	t_wait = t_wait[:int(t_wait.shape[0]/250) * 250]
         
-        plt.suptitle('validation info')
-        fig.savefig('val.png',format='png')
-
-        # time
-        fig = plt.figure(3)
-
-        ax = plt.subplot(411) # one record per 40 iterations ,
-        ax.plot(t_all, color[0+color_id], label='all_time')
-        #ax.set_xlabel('epoch')
-        ax.set_ylabel('overall')
-
-        ax = plt.subplot(412)
-        ax.plot(t_calc, color[0+color_id], label='train_time')
-        #ax.set_xlabel('epoch')
-        ax.set_ylabel('calc')
-
-        ax = plt.subplot(413)
-        ax.plot(t_comm, color[0+color_id], label='comm_time')
-        #ax.set_xlabel('epoch')
-        ax.set_ylabel('comm')
-
-        ax = plt.subplot(414)
-        ax.plot(t_wait, color[0+color_id], label='wait_time')
-        ax.set_xlabel('epoch')
-        ax.set_ylabel('wait')
+        	t_all = np.mean(t_all.reshape(-1, 250), axis=1)
+        	t_calc = np.mean(t_calc.reshape(-1, 250), axis=1)
+        	t_comm = np.mean(t_comm.reshape(-1, 250), axis=1)
+        	t_wait = np.mean(t_wait.reshape(-1, 250), axis=1)
+        	
+        	# time
+	        fig = plt.figure(3)
+	
+	        ax = plt.subplot(411) # one record per 40 iterations ,
+	        ax.plot(t_all, color[0+color_id], label='all_time')
+	        #ax.set_xlabel('epoch')
+	        ax.set_ylabel('overall')
+	
+	        ax = plt.subplot(412)
+	        ax.plot(t_calc, color[0+color_id], label='train_time')
+	        #ax.set_xlabel('epoch')
+	        ax.set_ylabel('calc')
+	
+	        ax = plt.subplot(413)
+	        ax.plot(t_comm, color[0+color_id], label='comm_time')
+	        #ax.set_xlabel('epoch')
+	        ax.set_ylabel('comm')
+	
+	        ax = plt.subplot(414)
+	        ax.plot(t_wait, color[0+color_id], label='wait_time')
+	        ax.set_xlabel('epoch')
+	        ax.set_ylabel('wait')
+	        
+	        plt.suptitle('time per 5120 images')
         
-        plt.suptitle('time per 5120 images')
+        if self.info_dict['epoch_time']!=[]:
+        	count_epoch, t_epoch = np.transpose(self.info_dict['epoch_time'])
+        	
+        	# epoch time
+	        fig = plt.figure(4) # in hour
+	
+	        plt.plot(t_epoch/3600.0, color[0+color_id], label='epoch_time')
+	        plt.xlabel('epoch')
+	        plt.ylabel('time per epoch')
+	
+	        plt.ylim([0,3])
+	        
+	        plt.suptitle('epoch time')
+        
+        if self.info_dict['lr']!=[]:
+        	count_lr , lr = np.transpose(self.info_dict['lr'])
 
-
-        # epoch time
-        fig = plt.figure(4) # in hour
-
-        plt.plot(t_epoch/3600.0, color[0+color_id], label='epoch_time')
-        plt.xlabel('epoch')
-        plt.ylabel('time per epoch')
-
-        plt.ylim([0,3])
-        
-        plt.suptitle('epoch time')
-        
-        
-        # learning rate
-        
-        fig = plt.figure(5)
-
-        plt.plot(lr, color[0+color_id], label='lr')
-        plt.xlabel('epoch')
-        plt.ylabel('learning rate')
-        
-        plt.ylim([0,0.1])
-        
-        plt.suptitle('learning rate')
+	        # learning rate
+	        
+	        fig = plt.figure(5)
+	
+	        plt.plot(lr, color[0+color_id], label='lr')
+	        plt.xlabel('epoch')
+	        plt.ylabel('learning rate')
+	        
+	        plt.ylim([0,0.1])
+	        
+	        plt.suptitle('learning rate')
         
 
         if show: plt.show()
