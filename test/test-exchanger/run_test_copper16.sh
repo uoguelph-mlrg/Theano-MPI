dc05fe92638e4a767bb0a6480fa817bdcc939f06
Nikhil Sapru <nsapru@cop1.copper.sharcnet>
2016-06-17 16:24:07 -0400

    Added testing to compare copper16 output to ar

---
+++ test/test-exchanger/run_test_copper16.sh
@@ -0,0 +1,9 @@
+source ../../run/set4theano.sh
+
+device0=gpu0
+numa0=0
+device1=gpu1
+numa1=0
+
+mpirun --mca mpi_common_cuda_event_max 10000 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 --prefix /opt/sharcnet/openmpi/1.8.7/intel-15.0.3/std -x PYTHONPATH=$PYTHONPATH -x PATH=$PATH -x CPATH=$CPATH -x LIBRARY_PATH=$LIBRARY_PATH -x LD_LIBRARY_PATH=$LD_LIBRARY_PATH -n 1 numactl -N $numa0 python -u test_copper16.py $device0 : \
+	   --mca mpi_common_cuda_event_max 10000 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 --prefix /opt/sharcnet/openmpi/1.8.7/intel-15.0.3/std -x PYTHONPATH=$PYTHONPATH -x PATH=$PATH -x CPATH=$CPATH -x LIBRARY_PATH=$LIBRARY_PATH -x LD_LIBRARY_PATH=$LD_LIBRARY_PATH -n 1 numactl -N $numa1 python -u test_copper16.py $device1
