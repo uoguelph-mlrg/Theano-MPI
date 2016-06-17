3b497b6a051a18a59ece803516c9f93f24a55ca8
Fei Mao <powerreactor@gmail.com>
2016-06-17 10:46:38 -0400

    setted mpi_warn_on_fork to 0
    avoiding fork() warning

--- run/run_bsp_workers.sh
+++ run/run_bsp_workers.sh
@@ -9,23 +9,23 @@ fi
 
 case $size in
 	1)
-		mpirun --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 1 --bind-to none python -u ../lib/BSP_Worker.py
+		mpirun --mca mpi_warn_on_fork 0 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 1 --bind-to none python -u ../lib/BSP_Worker.py
 		;;
 	2)
-		mpirun --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 2 --bind-to none python -u ../lib/BSP_Worker.py
+		mpirun --mca mpi_warn_on_fork 0 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 2 --bind-to none python -u ../lib/BSP_Worker.py
 		;;
 	4)
-		mpirun --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 4 --bind-to none python -u ../lib/BSP_Worker.py
+		mpirun --mca mpi_warn_on_fork 0 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 4 --bind-to none python -u ../lib/BSP_Worker.py
 		;;
 	8)
-		mpirun --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 8 --bind-to none python -u ../lib/BSP_Worker.py
+		mpirun --mca mpi_warn_on_fork 0 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 8 --bind-to none python -u ../lib/BSP_Worker.py
 		;;
 		
 	16)
 		host1=cop1
 		host2=cop2
-		mpirun --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 8 -host $host1 python -u ../lib/BSP_Worker.py : \
-			   --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 8 -host $host2 python -u ../lib/BSP_Worker.py
+		mpirun --mca mpi_warn_on_fork 0 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 8 -host $host1 python -u ../lib/BSP_Worker.py : \
+			   --mca mpi_warn_on_fork 0 --mca btl_smcuda_use_cuda_ipc 1 --mca mpi_common_cuda_cumemcpy_async 1 -n 8 -host $host2 python -u ../lib/BSP_Worker.py
 		;;
 		
 	
