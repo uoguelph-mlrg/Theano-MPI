bb112ea4e417fe4ebb8a1937b38a75fbbd5aeaf4
He <hma02@users.noreply.github.com>
2017-01-18 21:59:07 -0500

    predebugging of easgd

--- test/test-rule/theanompi/easgd_server.py
+++ test/test-rule/theanompi/easgd_server.py
@@ -9,7 +9,7 @@ class EASGD_Server(MPI_GPU_Process):
     def __init__(self, device):
         MPI_GPU_Process.__init__(self, device)
         
-        # self.worker_comm = {}
+        self.worker_gpucomm = {}
         self.worker_id = {}
         self.first_worker_id = None
         
@@ -19,24 +19,102 @@ class EASGD_Server(MPI_GPU_Process):
         reply = None
     
         if message in ['sync_register']:
+            
             if self.first_worker_id == None:
                 self.first_worker_id = worker_id
                 print '[Server] recording worker is %s' % worker_id
                 reply = 'first'
+            
+            self.worker_id[str(worker_rank)] = int(worker_id)
+            
+            print '[Server] registered worker %d' % worker_id
+            
+            return reply
+            
+        try:
+            valid = self.valid['%s' % worker_id]
+            amount = self.uidx['%s' % worker_id]
+            adj_lr = self.adj_lr['%s' % worker_id]
+        except KeyError:
+            self.valid['%s' % worker_id] = False
+            self.adj_lr['%s' % worker_id] = False
+            self.uidx['%s' % worker_id] = 0
+            self.adj_lr = self.adj_lr.fromkeys(self.adj_lr, True) # when a new worker joins
+            
+        # Training related requests
+        
+        if message == 'next':
+            
+            if self.start_time is None:
+                self.start_time = time.time()
+        
+            if sum(self.uidx.values()) >= self.max_mb: # stop when finish all epochs
+                print "[Server] Total training time %.2fh" % ((time.time() - self.start_time)/3600.0)
+                reply = 'stop'
+        
+            elif self.valid['%s' % worker_id]:
+                self.valid['%s' % worker_id] = False
+                reply = 'val'
+        
+            elif self.adj_lr['%s' % worker_id]:
+                self.adj_lr['%s' % worker_id] = False
+                reply = 'adjust_lr'
+        
+            else:
+                reply = 'train' 
+        
+        elif 'done' in message:
+    
+            self.uidx['%s' % worker_id] += message['done']
+            #print '[Server] uidx %d' % sum(self.uidx.values())
+
+        elif message == 'uepoch':
+
+            reply = [self.uepoch, len(self.worker_comm)]
+        
+        if message in ['next', 'uepoch'] or 'done' in message:       
+    
+            now_uidx = sum(self.uidx.values())
+            self.uepoch = int(now_uidx/self.validFreq)
+            if self.last_uepoch != self.uepoch:
+                #print "[Server] now global epoch %d" % self.uepoch
+                self.last_uepoch = self.uepoch 
+                self.adj_lr = self.adj_lr.fromkeys(self.adj_lr, True) # when a epoch is finished
+                #self.valid = self.valid.fromkeys(self.valid, True)
+                self.valid["%s" % self.first_worker_id] = True # only the first worker validates
+        
+                # tunning server alpha
+                a_step1, a_step2, a_step3 = self.config['alpha_step']
+                if self.uepoch>a_step1 and self.uepoch< a_step2:
+                    step_idx = 1
+                elif self.uepoch>a_step2 and self.uepoch< a_step3:
+                    step_idx = 2
+                elif self.uepoch>a_step3:
+                    step_idx = 3
+                else:
+                    step_idx = 0
+                self.exchanger.alpha=self.config['server_alpha'] - self.config['alpha_minus']*step_idx
+                print 'server alpha changed to %f' % self.exchanger.alpha
+        
+        
+            if self.last == None:
+                self.last = float(time.time())
+        
+            if now_uidx - self.last_uidx >= 400:
+        
+                now = float(time.time())
+
+                print '[Server] %d time per 5120 images: %.2f s' % \
+                        (self.uepoch, (now - self.last)/(now_uidx - self.last_uidx)*40.0)
+
+                self.last_uidx = now_uidx
+                self.last = now
     
         return reply
     
     def action_after(self, worker_id,  worker_rank, message):
         
-        if 'sync_register' in message: # Connecting synchronously started workers
-        
-            worker_rank = self.comm.recv(source = MPI.ANY_SOURCE, tag=int(worker_id))
-        
-            self.worker_rank[str(worker_id)] = int(worker_rank)
-        
-            print '[Server] registered worker %d' % worker_id
-        
-        elif message == 'disconnect':
+        if message == 'disconnect':
 
             self.worker_comm.pop(str(worker_id))
         
@@ -48,9 +126,44 @@ class EASGD_Server(MPI_GPU_Process):
             
             import sys
             sys.exit(0)
+            
+            
+        if message in ['sync_register']:
+            
+            gpucomm = self.get_intranode_comm_pair(worker_rank)
+            
+            self.worker_comm[str(worker_id)]= gpucomm
+            
+        elif message == 'exchange':
+    
+            self.exchanger.gpucomm = self.worker_comm[str(worker_id)]
+            # self.exchanger.dest = worker_rank
+    
+            self.exchanger.exchange()
+    
+        elif message == 'copy_to_local':
+    
+            self.exchanger.gpucomm = self.worker_comm[str(worker_id)]
+            # self.exchanger.dest = worker_rank
+    
+            self.exchanger.copy_to_local()
         
+    
+    def build(self, model):
+        
+        from theanompi.lib.helper_funcs import check_model
+        
+        # check model has necessary attributes
+        check_model(model)
+        
+        # choose the type of exchanger
+        from theanompi.lib.exchanger import EASGD_Exchanger
+        self.exchanger = EASGD_Exchanger(self.config, 
+                                    self.drv, 
+                                    self.model.params, 
+                                    etype='server')
                 
-    def test_run(self):
+    def EASGD_run(self):
         
         if self.comm == None:
             
@@ -79,6 +192,7 @@ class EASGD_Server(MPI_GPU_Process):
             self.action_after(request['id'],request['rank'], \
                                                     request['message'])
                                                     
+                                                    
 if __name__ == '__main__':
     
     import sys
@@ -88,4 +202,11 @@ if __name__ == '__main__':
 
     server = EASGD_Server(device)
     
-    server.test_run()
+    import importlib
+    mod = importlib.import_module(modelfile)
+    modcls = getattr(mod, modelclass)
+    model = modcls(config)
+    
+    server.build(model)
+    
+    server.EASGD_run(model)
--- test/test-rule/theanompi/easgd_worker.py
+++ test/test-rule/theanompi/easgd_worker.py
@@ -54,28 +54,6 @@ class EASGD_Worker(MPI_GPU_Process):
         print 'on rank %d, pair %s generated' % (self.interrank, pair)
         
         
-    def build(self, model, config):
-        
-        from theanompi.lib.helper_funcs import check_model
-        
-        # check model has necessary attributes
-        check_model(model)
-        
-        # construct model train function based on sync rule
-        model.compile_iter_fns()
-        
-        self.verbose = (self.rank==0)
-        from theanompi.lib.recorder import Recorder
-        self.recorder = Recorder(self.comm, printFreq=40, 
-                                 modelname=model.name, verbose=self.verbose)
-        
-        # choose the type of exchanger
-        from theanompi.lib.exchanger import EASGD_Exchanger
-        self.exchanger = EASGD_Exchanger(self.comm, self.gpucomm, 
-                                            self.exch_strategy, self.sync_type, 
-                                            self.ctx, model)
-        
-        
     def comm_request(self, message):
         
         if self.comm == None:
@@ -94,7 +72,7 @@ class EASGD_Worker(MPI_GPU_Process):
         return reply
 
         
-    def comm_action(self, message, action=None):
+    def comm_action(self, message, action=None, action_args=None):
         
         if self.comm == None:
             
@@ -108,7 +86,14 @@ class EASGD_Worker(MPI_GPU_Process):
         
         reply = self.comm.recv(source=self.server_rank, tag=200)
         
-        if action: action()
+        if action: action(action_args)
+        
+        
+    def register_worker(self):
+        
+        first = self.comm_request('sync_register')
+        
+        self.verbose = (first == 'first')
             
     def test_run(self):
         
@@ -119,6 +104,33 @@ class EASGD_Worker(MPI_GPU_Process):
         print 'success', reply
         
         
+    def build(self, model, config):
+        
+        from theanompi.lib.helper_funcs import check_model
+        
+        # check model has necessary attributes
+        check_model(model)
+        
+        # construct model train function based on sync rule
+        model.compile_iter_fns()
+        
+        from theanompi.lib.recorder import Recorder
+        self.recorder = Recorder(self.comm, printFreq=40, 
+                                 modelname=model.name, verbose=self.verbose)
+        
+        # choose the type of exchanger
+        from theanompi.lib.exchanger import EASGD_Exchanger
+        self.exchanger = EASGD_Exchanger(self.comm, self.gpucomm,
+                                         self.ctx, model, etype='worker')
+                                         
+        #TODO reimplement EASGD using pygpu.bcast
+    
+    def copy_to_local(self):
+        
+        self.comm_action(message = 'copy_to_local', \
+                    action=self.exchanger.copy_to_local)
+        if self.verbose: print '\nSynchronized param with server'
+                
     def EASGD_run(self, model):
         
         # after the barrier, run asynchronously
@@ -129,53 +141,82 @@ class EASGD_Worker(MPI_GPU_Process):
         snapshot_path = './snapshots/'
         recorder=self.recorder
         exchanger=self.exchanger
+        epoch_start = False
+        batch_i=0
+        self.uepoch=0
         
         from theanompi.lib.helper_funcs import save_model
-
-        for epoch in range(model.n_epochs):
-            
-            model.epoch=epoch
-            
-            recorder.start_epoch()
-            
-            # train
-    
-            for batch_i in range(model.data.n_batch_train):
         
-                model.train_iter(batch_i, recorder)
+        
+        while True:
+            
+            mode = self.comm_request('next')
+            
+            if mode == 'train':
                 
-                if batch_i % exchange_freq == 0: 
-                    exchanger.exchange(recorder)
-        
-                recorder.print_train_info(batch_i * self.size)
-            
-            model.reset_iter('train')
-        
-            # val
-            
-            self.comm.Barrier()
-    
-            for batch_j in range(model.data.n_batch_val):
-        
-                model.val_iter(batch_i, recorder)
+                if epoch_start == False:
+                    recorder.start_epoch()
+                    epoch_start = True
+                    
+                    
+                for i in range(exchange_freq):
+                    
+                    model.train_iter(batch_i, recorder)
+                    batch_i+=1
+                    recorder.print_train_info(batch_i)
+                    
+                self.comm_request(dict(done=self.train_len))
 
+                self.comm_action(message = 'exchange', \
+                            action=self.exchanger.exchange, action_args=recorder)
                 
-            model.reset_iter('val')
-            
-            recorder.gather_val_info()
+            elif mode == 'adjust_hyperp':
+                
+                model.adjust_hyperp(self.uepoch)
+                
+            elif mode == 'val':
+                
+                self.copy_to_local()
+                
+                model.reset_iter('val')
+                
+                for batch_j in range(model.data.n_batch_val):
         
-            recorder.print_val_info(batch_i)
-            model.current_info = recorder.get_latest_val_info()
-            
-            if self.rank==0: recorder.save(batch_i, model.shared_lr.get_value())
-            
-            if epoch % snapshot_freq == 0 and self.rank==0: save_model(model, snapshot_path, verbose=self.verbose)
-            
-            model.adjust_hyperp(epoch)
-            
-            recorder.end_epoch(batch_i, epoch)
-            
+                    model.val_iter(self.uepoch, recorder)
+                    
+                model.reset_iter('val')
+                
+                recorder.print_val_info(batch_i)
+                
+                model.current_info = recorder.get_latest_val_info()
+                
+                if self.verbose: recorder.save(batch_i, model.shared_lr.get_value())
+    
+                self.uepoch, n_workers = self.comm_request('uepoch')
+                
+                if self.verbose and self.uepoch % snapshot_freq == 0: 
+                    save_model(model, snapshot_path, verbose=self.verbose)
+                    
+                self.copy_to_local()
+                
+                
+                if epoch_start == True:
+                    recorder.end_epoch(batch_i, self.uepoch)
+                    epoch_start = False
+                    
+            elif mode='stop':
+                
+                self.copy_to_local()
+                
+                if epoch_start == True:
+                    recorder.end_epoch(batch_i, self.uepoch)
+                    epoch_start = False
+                    
+                break
+                            
         model.cleanup()
+            
+        
 
         
 if __name__ == '__main__':
