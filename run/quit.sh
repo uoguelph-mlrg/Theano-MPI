254ea9ada4c7a60f97031e871f1df3b6f9c6a065
He <hma02@users.noreply.github.com>
2017-01-18 22:16:20 -0500

    replace old structure with the new one

--- run/quit.sh
+++
@@ -1,7 +0,0 @@
-screen -X -S server quit
-screen -X -S worker0 quit
-screen -X -S worker1 quit
-screen -X -S worker2 quit
-screen -X -S worker3 quit
-
-pkill -9 -f mpirun
--- run/sub_command_bsp.sh
+++
@@ -1,7 +0,0 @@
-# source ./set4theano-new.sh
-num_worker=$1
-unit='g'
-mem=$((num_worker*10))$unit
-ncpu=$((num_worker*2))
-
-sqsub -q gpu -f mpi -n $ncpu -r 7d -o %J.out --gpp=$num_worker --mpp=$mem --nompirun ./run_bsp_workers.sh $num_worker
