7c7d98e728644f7353b4612f7c5279dd7645c785
He <hma02@users.noreply.github.com>
2017-01-30 14:02:57 -0500

    update show_inforec.py

--- examples/test_bsp.py
+++ examples/test_bsp.py
@@ -9,7 +9,7 @@ rule=BSP()
 
 # modelfile: the relative path to the model file
 # modelclass: the class name of the model to be imported
-rule.init(devices=['cuda1', 'cuda2'] , 
+rule.init(devices=['cuda0', 'cuda1'] , 
           modelfile = 'theanompi.models', 
           modelclass = 'AlexNet') 
 rule.wait()
